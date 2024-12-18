fc976ab601bc9f1ec83e54ac668b4b26b66b2cb0
Eic17H <139933730+Eic17H@users.noreply.github.com>
2024-12-18 12:51:11 +0100

    Ordine nel file esempio delle pipe

--- Strutture/pipe.c
+++ Strutture/pipe.c
@@ -1,10 +1,12 @@
 #include "pipe.h"
 
+// Usa dati nel padre
 void use(Data* data, int lung){
     for(int i=0; i<lung-1; i++) printf("%d ", data[i]);
     printf("%d\n", data[lung-1]);
 }
 
+// Genera dati nel figlio
 void gen(Data* data, int lung){
     for(int i=0; i<lung; i++) data[i] = rand()%10;
 }
@@ -13,6 +15,7 @@ int main(){
     int pipe_fds[2];
     Data through[LUNG];
 
+    // Se non riesce a fare la pipeS
     if(pipe(pipe_fds) == -1) {
         perror("Pipe call");
         exit(1);
@@ -20,20 +23,23 @@ int main(){
 
     pid_t pid;
     pid=fork();
+
+    // Se non riesce a fare la fork
     if(pid < 0) {perror("fork call"); _exit(2);}
-    else if(pid!=0) while(1){
-        // Padre: gestione generale
-        while(1){
-            close(pipe_fds[1]);
-            read(pipe_fds[0], through, LUNG*sizeof(Data));
-            use(through, LUNG);
-        }
-    }else{
-        // Figlio: guardia
+
+    else if(pid==0){
+        // Figlio: genera dati e li mette nella pipe
         while(1){
             gen(through, LUNG);
             close(pipe_fds[0]);
             write(pipe_fds[1], through, LUNG*sizeof(Data));
+        }
+    }else{
+        // Padre: legge dati dalla pipe e li usa
+        while(1){
+            close(pipe_fds[1]);
+            read(pipe_fds[0], through, LUNG*sizeof(Data));
+            use(through, LUNG);
         }
     }
 }
